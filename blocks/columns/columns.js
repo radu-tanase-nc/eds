35840016d49c1684093a14a3f7334ea17bee05d8
Radu Tanase <radu.tanase@netcentric.biz>
2025-06-23 13:35:03 +0300

    Added new page with Accordion

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,12 +1,7 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
-  const rating = block.querySelectorAll('p')[1];
-      let newString = '';
-      for(let i = 0; i < rating.innerHTML; i++) {
-          newString = newString + '*';
-      }
-      rating.innerHTML = newString;
+
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
